7d5576962118ed1f02ff5aa6cdee22a2e96f75e5
radiopushka <48099825+radiopushka@users.noreply.github.com>
2022-11-20 02:54:39 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <exception>
 #include <unistd.h>
 #include<thread>
+#include "microphone/microphone.cpp"
+
 //requires: vorbis pthread ncurses alsasound
 PLmanager pm(2);
 playlistmanager p;
@@ -12,6 +14,7 @@ volatile int putname=0;
 string nowplaying="";
 string path="";
 Mixer m;
+Microphone mic;
 //orignal code by Evan Nikitin
 void songs( Mixer* m){
 	if(pm.canNext()==-1){sleep(1);//one second delay
@@ -51,14 +54,21 @@ void playSongs(){//song playing thread
  }	
  m.closeMixer();
 }
+void microphone(){
+ mic.runner();
+}
 void mainInterface(){//ncurses main screen
  int c=0;
+ int mi=0;
  while(c!='q'){
   clear();
   while(putname==1){}
   printw("Now Playing: %s\n",nowplaying.c_str());
   line();
-  printw("press p to manage playlists, press any key to update \"Now Playing\", press s to pause, press a to skip\n");
+  printw("press p to manage playlists, press any key to update \"Now Playing\", press s to pause, press a to skip, press m to toggle ");
+  if(mi==1){attron(A_STANDOUT);}
+  printw("microphone\n");
+  if(mi==1){attroff(A_STANDOUT);}
   c=getch();	
   if(c=='a'){
 	m.pause();
@@ -66,6 +76,10 @@ void mainInterface(){//ncurses main screen
 	m.unpause();
 	
   } 
+  if(c=='m'){
+	mic.toggle();  
+	if(mi==0){mi=1;}else{mi=0;}
+  }
   if(c=='s'){
 	if(m.PAUSE==1){
 	 m.PAUSE=0;
@@ -83,6 +97,7 @@ void mainInterface(){//ncurses main screen
 int main(){
 	srand(time(NULL));//random initialization
 	thread fplayer(playSongs);//queu and playback thread
+	thread micro(microphone);//microphone thread
 	setlocale(LC_ALL, "");
 	initscr();
 	noecho();
@@ -99,6 +114,7 @@ int main(){
     curs_set(s);
     
     exit_curses(0);
+    mic.kill();
     m.PAUSE=1;
     m.pause();
     m.closeMixer();
